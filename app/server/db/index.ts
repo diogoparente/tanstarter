c6a24183ca1bd51f7a8800ddab41da09417d4b91
Diogo Parente <hello@imdiogo.com>
2024-11-16 22:26:30 +0000

    chore: initial db migration

--- app/server/db/index.ts
+++ app/server/db/index.ts
@@ -1,9 +1,21 @@
-import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import {
+  drizzle as RemoteDrizzle,
+  drizzle as LocalDrizzle,
+  type PostgresJsDatabase,
+} from "drizzle-orm/postgres-js";
 
 import * as schema from "./schema";
 
 const driver = postgres(process.env.DATABASE_URL as string);
 
-export const db = drizzle({ client: driver, schema });
-export const table = schema;
+let db: PostgresJsDatabase<typeof schema> | null = null;
+
+if (process.env.NODE_ENV === "production") {
+  db = RemoteDrizzle({ client: driver, schema });
+} else {
+  const migrationClient = postgres(process.env.DATABASE_URL as string);
+  db = LocalDrizzle(migrationClient);
+}
+
+export { db };
